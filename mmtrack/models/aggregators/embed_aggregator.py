95b8cf63d7e36d6e7854c26a7db69a09075e1d4d
ypilseong <104025187+ypilseong@users.noreply.github.com>
2024-06-26 15:41:43 +0900

    Update embed_aggregator.py

--- mmtrack/models/aggregators/embed_aggregator.py
+++ mmtrack/models/aggregators/embed_aggregator.py
@@ -2,7 +2,7 @@
 import torch
 import torch.nn as nn
 from mmcv.cnn.bricks import ConvModule
-from mmcv.runner import BaseModule
+from mmengine.model import BaseModule
 
 from ..builder import AGGREGATORS
 
